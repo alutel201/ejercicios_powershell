35933b5b4dbe5e6d58321076050db845f53fdd4b
Jaume <alutel201@ieselcaminas.org>
2023-09-28 09:18:56 +0200

    practica4

---
+++ practica4/extra.ps1
@@ -0,0 +1,8 @@
+﻿[int] $altura = Read-Host "Introduce un numero entero: "
+for($i = 1; $i -le $altura; $i++){
+    for($num = $i; $num -ge 1; $num =$num - 1){
+    Write-Host -NoNewline "$num "
+    
+    }
+    Write-Host ""
+}
---
+++ practica4/extra2.ps1
@@ -0,0 +1,6 @@
+﻿$altura = Read-Host "Introduce la altura de la piramide(un numero entero): "
+for($i = 1; $i -le $altura; $i++){
+    Write-Host ("- " *($i-1)) -NoNewline
+    Write-Host ("*")
+    
+}
