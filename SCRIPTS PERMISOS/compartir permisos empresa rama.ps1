e216e5b8530842490b06bee57db736266c75f530
Jaume <alutel201@ieselcaminas.org>
2023-10-17 12:59:23 +0200

    SCRIPTS PERMISOSç

---
+++ SCRIPTS PERMISOS/compartir permisos empresa rama.ps1
@@ -0,0 +1,39 @@
+﻿New-SmbShare -name "Personal" -Path "C:\Empresa\Personal"
+$acl = Get-Acl "C:\Empresa\Personal"
+$rule1 = New-Object System.Security.AccessControl.FileSystemAccessRule("Personal","Modify","ContainerInherit,ObjectInherit","None","Allow")
+$rule2 = New-Object System.Security.AccessControl.FileSystemAccessRule("Todos","Read","ContainerInherit,ObjectInherit","None","Allow")
+$rule3 = New-Object System.Security.AccessControl.FileSystemAccessRule("Administradores","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.SetAccessRule($rule1)
+$acl.SetAccessRule($rule2)
+$acl.SetAccessRule($rule3)
+Set-Acl "C:\Empresa\Personal" $acl
+
+New-SmbShare -name "Finanzas" -Path "C:\Empresa\Finanzas"
+$acl = Get-Acl "C:\Empresa\Finanzas"
+$rule1 = New-Object System.Security.AccessControl.FileSystemAccessRule("Finanzas","Modify","ContainerInherit,ObjectInherit","None","Allow")
+$rule2 = New-Object System.Security.AccessControl.FileSystemAccessRule("Todos","Read","ContainerInherit,ObjectInherit","None","Allow")
+$rule3 = New-Object System.Security.AccessControl.FileSystemAccessRule("Administradores","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.SetAccessRule($rule1)
+$acl.SetAccessRule($rule2)
+$acl.SetAccessRule($rule3)
+Set-Acl "C:\Empresa\Finanzas" $acl
+
+New-SmbShare -name "Ventas" -Path "C:\Empresa\Ventas"
+$acl = Get-Acl "C:\Empresa\Ventas"
+$rule1 = New-Object System.Security.AccessControl.FileSystemAccessRule("Ventas","Modify","ContainerInherit,ObjectInherit","None","Allow")
+$rule2 = New-Object System.Security.AccessControl.FileSystemAccessRule("Todos","Read","ContainerInherit,ObjectInherit","None","Allow")
+$rule3 = New-Object System.Security.AccessControl.FileSystemAccessRule("Administradores","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.SetAccessRule($rule1)
+$acl.SetAccessRule($rule2)
+$acl.SetAccessRule($rule3)
+Set-Acl "C:\Empresa\Ventas" $acl
+
+New-SmbShare -name "Produccion" -Path "C:\Empresa\Produccion"
+$acl = Get-Acl "C:\Empresa\Produccion"
+$rule1 = New-Object System.Security.AccessControl.FileSystemAccessRule("Produccion","Modify","ContainerInherit,ObjectInherit","None","Allow")
+$rule2 = New-Object System.Security.AccessControl.FileSystemAccessRule("Todos","Read","ContainerInherit,ObjectInherit","None","Allow")
+$rule3 = New-Object System.Security.AccessControl.FileSystemAccessRule("Administradores","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.SetAccessRule($rule1)
+$acl.SetAccessRule($rule2)
+$acl.SetAccessRule($rule3)
+Set-Acl "C:\Empresa\Produccion" $acl
---
+++ SCRIPTS PERMISOS/compartr empresa.ps1
@@ -0,0 +1,5 @@
+﻿New-SmbShare -name "Empresa" -Path "C:\Empresa"
+$acl = Get-Acl "C:\Empresa"
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Todos","Read","None","None","Allow")
+$acl.SetAccessRule($rule)
+Set-Acl "C:\Empresa" $acl
